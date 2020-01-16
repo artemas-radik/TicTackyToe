19c9363b3a5e1870bfc5834e0778dca7d4e6dff5
Artemas Radik <artemasr21@bronxvilleschool.org>
2020-01-15 21:26:07 -0500

    functional but x and o are missing

--- TicTackyToe/ViewController.swift
+++ TicTackyToe/ViewController.swift
@@ -14,17 +14,55 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var topLabel: UILabel!
     @IBOutlet var board: [DesignableButton]!
+    var gameBoardIsLocked = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         overrideUserInterfaceStyle = .dark
         topLabel.font = topLabel.font.withSize( (3.6/71) * UIScreen.main.bounds.height)
         resetButton(nil)
     }
 
+    @IBAction func boardButtonWasPressed(_ sender: DesignableButton) {
+        let col = sender.tag % 3
+        let row = sender.tag / 3
+        
+        if gameBoardIsLocked {return}
+        if !brain.checkForLegalMoveAtRowAndCol(row: row, col: col) {return}
+        brain.placeTokenAtRowAndCol(row: row, col: col)
+        sender.titleLabel!.text = "hi"
+        sender.backgroundColor = brain.getCurrentPlayer() == "X" ? UIColor.systemPink : UIColor.systemGreen
+        
+        if brain.checkForDraw() {
+            topLabel.text = "Draw!"
+            topLabel.textColor = UIColor.label
+            gameBoardIsLocked = true
+            return
+        }
+        
+        if brain.checkForWinner() {
+            topLabel.text = "\(brain.getCurrentPlayer()) Won!"
+            gameBoardIsLocked = true
+            return
+        }
+
+        brain.switchPlayer()
+        updateTopLabelForNextMove()
+    }
+    
     @IBAction func resetButton(_ sender: Any?) {
         brain.resetBrain()
+        gameBoardIsLocked = false
+        
+        for button in board {
+            button.backgroundColor = UIColor.secondarySystemFill
+            button.titleLabel!.text = ""
+        }
+        
+        updateTopLabelForNextMove()
+    }
+    
+    func updateTopLabelForNextMove() {
         topLabel.text = "\(brain.getCurrentPlayer())'s Move"
         topLabel.textColor = brain.getCurrentPlayer() == "X" ? UIColor.systemPink : UIColor.systemGreen
     }
