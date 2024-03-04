774b121c822aebb82def930aedb15fdf8e56c815
Artemas Radik <ajradik@gmail.com>
2024-03-04 05:18:56 -0500

    fix entitlements

--- Shared/TicTacToeApp.swift
+++ Shared/TicTacToeApp.swift
@@ -2,6 +2,15 @@ import SwiftUI
 
 @main
 struct TicTacToeApp: App {
+    
+    #if os(iOS)
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    #endif
+    
+    #if os(macOS)
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    #endif
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
