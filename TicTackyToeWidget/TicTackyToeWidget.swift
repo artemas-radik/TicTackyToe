bc83e3822b1f0a16a7e03d355c1492fd753685f9
Artemas Radik <ajradik@gmail.com>
2024-03-04 04:19:27 -0500

    app widget working

--- TicTackyToeWidget/TicTackyToeWidget.swift
+++ TicTackyToeWidget/TicTackyToeWidget.swift
@@ -7,6 +7,19 @@
 
 import WidgetKit
 import SwiftUI
+import AppIntents
+
+@available(iOS 16.0, macOS 13.0, watchOS 9.0, tvOS 16.0, *)
+struct SuperCharge: AppIntent {
+    
+    static var title: LocalizedStringResource = "Emoji Ranger SuperCharger"
+    static var description = IntentDescription("All heroes get instant 100% health.")
+    
+    func perform() async throws -> some IntentResult {
+        print("hiiiiii")
+        return .result()
+    }
+}
 
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
@@ -41,12 +54,24 @@ struct SimpleEntry: TimelineEntry {
 
 struct TicTackyToeWidgetEntryView : View {
     var entry: Provider.Entry
+    
+    @State var text = "hello"
 
     var body: some View {
         VStack {
             HStack {
-                Text("Time:")
+                Text(text)
                 Text(entry.date, style: .time)
+            }
+            
+            if #available(macOS 14.0, iOS 17.0, *) {
+                HStack(alignment: .top) {
+                    Button(intent: SuperCharge()) {
+                        Image(systemName: "bolt.fill")
+                    }
+                }
+                .tint(.white)
+                .padding()
             }
 
             Text("Emoji:")
