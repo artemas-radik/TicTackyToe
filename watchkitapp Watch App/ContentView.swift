48b4451566a8547d3ed868a6eb3a4425aa0bcf09
Artemas Radik <ajradik@gmail.com>
2024-03-04 19:51:19 -0500

    init watch app

---
+++ watchkitapp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  watchkitapp Watch App
+//
+//  Created by Artemas on 3/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ watchkitapp Watch App/watchkitappApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchkitappApp.swift
+//  watchkitapp Watch App
+//
+//  Created by Artemas on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct watchkitapp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
